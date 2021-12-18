1ad1d7a9b8777d9593971fa33db10956a87883a4
xalll <farisikhlasulhaq@yahoo.co.id>
2021-12-19 00:21:31 +0700

    Profil Testing 1 (Faris)

--- cypress/integration/6-FarisIkhlasulHaq/profil-testing1.spec.js
+++ cypress/integration/6-FarisIkhlasulHaq/profil-testing1.spec.js
@@ -20,26 +20,55 @@ describe('Mengisi Data Profil Dengan Sesuai (Benar)', () => {
         cy.get('#actionmenuaction-5').click()
             // Melakukan visit pada link berikut karena tipe id yang digunakan berupa link
         cy.visit('http://lms.jti.polinema.ac.id/user/edit.php?id=2218&amp;course=1')
-            // Melakukan clear isi tabel yang telah ada sebelumya
+            //Check Format Pada Firstname
+        cy.get('#id_firstname')
+            .should('have.class', 'form-control')
+            .and('have.attr', 'type')
+            // Melakukan clear isi tabel yang telah ada sebelumya\
         cy.get('#id_firstname')
             .clear().should('be.empty')
-            // Menambahkan isi dari kolom yang telah dihapus sebelumnya (firstname)
-        cy.get('#id_firstname')
-            .type('1332123212').should('not.have.text', "/^[^1-9]*$/")
+            // Menambahkan Nama
+        cy.get('#id_firstname').type(userID_Alpha_Numeric()).should('not.have.text', /^[^1-9]*$/)
+            // Random Name
+        function userID_Alpha_Numeric() {
+            var text = "";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+            for (var i = 0; i < 10; i++)
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+            return text;
+        }
+        // Menambahkan isi dari kolom yang telah dihapus sebelumnya (firstname)
+        expect('#id_firstname').to.not.equal('/^1-9/')
+            //Check Format Pada Lastname
+        cy.get('#id_lastname')
+            .should('have.class', 'form-control')
+            .and('have.attr', 'type')
             // Melakukan clear isi tabel yang telah ada sebelumya (lastname)
         cy.get('#id_lastname')
             .clear().should('be.empty')
             // Menambahkan isi dari kolom yang telah dihapus sebelumnya (lastname)
         cy.get('#id_lastname')
-            .type('Ikhlasul').should('not.have.value', /^[^1-9]*$/)
-            // Melakukan clear isi tabel yang telah ada sebelumya (kota)
+            .type(userID_Alpha_Numeric_Lastname).should('not.have.value', /^[^1-9]*$/)
+            // Random Name
+        function userID_Alpha_Numeric_Lastname() {
+            var text = "";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+            for (var i = 0; i < 10; i++)
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+            return text;
+        }
+        // Melakukan clear isi tabel yang telah ada sebelumya (kota)
         cy.get('#id_country')
             .select('Indonesia').should('have.value', 'ID')
         cy.get('#id_city')
             .clear().should('be.empty')
             // Menambahkan isi dari kolom yang telah dihapus sebelumnya (kota)
         cy.get('#id_city')
-            .type('6767283').invoke('text')
+            .type('Malang').invoke('text')
             .should('match', /^[^a-z]*$/)
             // Menekan submit button
         cy.get('#id_submitbutton').should('have.value', 'Update profile')
